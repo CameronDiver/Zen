4e26837ded3f075510e451727220e18862ec2ac1
Cameron Diver <cameron@0xcd.dev>
2020-05-20 07:35:16 +0100

    Fix while loops always being entered for the first iteration
    Also fix a broken test which also exhibited this issue
    
    Signed-off-by: Cameron Diver <cameron@0xcd.dev>

--- src/Language/Zen/CodeGen/Statement.hs
+++ src/Language/Zen/CodeGen/Statement.hs
@@ -23,37 +23,31 @@ codegenStatement stmt =
 -- up my editor and formatters to no end) but that gives an
 -- inifinite loop which I was unable to debug successfully
 codegenIfStatement :: SAStatement -> Codegen ()
-codegenIfStatement (SAIfStatement predicate iBody eBody) = mdo
-  bool <- codegenExpr predicate
-  L.condBr bool thenBlock elseBlock
-
-  thenBlock <- L.block `L.named` "then"
-  mapM_ codegenStatement iBody
-  mkTerminator $ L.br mergeBlock
-
-  elseBlock <- L.block `L.named` "else"
-  mapM_ codegenStatement eBody
-  mkTerminator $ L.br mergeBlock
-
-  mergeBlock <- L.block `L.named` "merge"
-  pure ()
+codegenIfStatement (SAIfStatement predicate iBody eBody) =
+  mdo bool <- codegenExpr predicate
+      L.condBr bool thenBlock elseBlock
+      thenBlock <- L.block `L.named` "then"
+      mapM_ codegenStatement iBody
+      mkTerminator $ L.br mergeBlock
+      elseBlock <- L.block `L.named` "else"
+      mapM_ codegenStatement eBody
+      mkTerminator $ L.br mergeBlock
+      mergeBlock <- L.block `L.named` "merge"
+      pure ()
 codegenIfStatement _ = error "If statement not passed to generator for if"
 
 codegenWhileStatement :: SAStatement -> Codegen ()
-codegenWhileStatement (SAWhileStatement predicate body) = mdo
-
-  start <- codegenExpr predicate
-  L.condBr start whileBlock mergeBlock
-
-  L.br whileBlock
-  whileBlock <- L.block `L.named` "while_body"
-  mapM_ codegenStatement body
-  continue <- codegenExpr predicate
-  mkTerminator $ L.condBr continue whileBlock mergeBlock
-
-  mergeBlock <- L.block `L.named` "merge"
-  pure ()
-codegenWhileStatement _ = error "While statement not passed to generator for while"
+codegenWhileStatement (SAWhileStatement predicate body) =
+  mdo start <- codegenExpr predicate
+      L.condBr start whileBlock mergeBlock
+      whileBlock <- L.block `L.named` "while_body"
+      mapM_ codegenStatement body
+      continue <- codegenExpr predicate
+      mkTerminator $ L.condBr continue whileBlock mergeBlock
+      mergeBlock <- L.block `L.named` "merge"
+      pure ()
+codegenWhileStatement _ =
+  error "While statement not passed to generator for while"
 
 mkTerminator :: Codegen () -> Codegen ()
 mkTerminator instr = do
